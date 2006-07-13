3c4c4ee453a53183305410a98792aacceaefabe5
whipple <whipple>
2006-07-13 17:25:08 +0000

    made it work

--- xdg-utils/tests/xdg-desktop-menu/data/shinythings_example/install-webmirror.sh
+++ xdg-utils/tests/xdg-desktop-menu/data/shinythings_example/install-webmirror.sh
@@ -9,11 +9,6 @@ if [ -z "$p" ] ; then
 	p='--user'
 fi
 
-xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror.desktop
-xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror-admin.desktop
-
-xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror.directory
-
 xdg-icon-resource "$c" "$p" --size 22 ../../../icons/red-22.png webmirror.png
 xdg-icon-resource "$c" "$p" --size 22 ../../../icons/red-22.png webmirror-menu.png
 xdg-icon-resource "$c" "$p" --size 22 ../../../icons/red-22.png webmirror-admintool.png
@@ -21,4 +16,12 @@ xdg-icon-resource "$c" "$p" --size 64 ../../../icons/red-64.png webmirror.png
 xdg-icon-resource "$c" "$p" --size 64 ../../../icons/red-64.png webmirror-menu.png
 xdg-icon-resource "$c" "$p" --size 64 ../../../icons/red-64.png webmirror-admintool.png
 
-xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror.menu
+#xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror.desktop
+#xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror-admin.desktop
+#xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror.directory
+#xdg-desktop-menu "$c" "$p" --vendor shinythings ./webmirror.menu
+
+xdg-desktop-menu "$c" "$p" ./webmirror.desktop
+xdg-desktop-menu "$c" "$p" ./webmirror-admin.desktop
+xdg-desktop-menu "$c" "$p" ./webmirror.directory
+xdg-desktop-menu "$c" "$p" ./webmirror.menu
